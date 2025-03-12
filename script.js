3983dd0f90c4a048a95a91dc0f750f8ae141a77c
alexandre lopes dos santos <alexandrelopes.info@gmail.com>
2025-03-12 08:43:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -21,8 +21,8 @@ const db = getFirestore(app);
 const users = {
   "alexandre": "2013",
   "clovis": "2028",
-  "patricio": "2015"
-  "roberto": "2099"
+  "patricio": "2015",
+  "roberto": "2099",
 };
 
 window.login = async function login() {
